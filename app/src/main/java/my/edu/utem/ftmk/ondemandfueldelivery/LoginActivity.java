66881065581b0db60f68abb3211ea34ffd3766d5
Syaz <zanie455@gmail.com>
2022-01-15 23:55:34 +0800

    Added LoginActivity

--- app/src/main/java/my/edu/utem/ftmk/ondemandfueldelivery/LoginActivity.java
+++ app/src/main/java/my/edu/utem/ftmk/ondemandfueldelivery/LoginActivity.java
@@ -3,12 +3,38 @@ package my.edu.utem.ftmk.ondemandfueldelivery;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            toMapActivity();
+        }
+    }
+
+    public void toMapActivity(View v){
+
+
+
+    }
+
 }
