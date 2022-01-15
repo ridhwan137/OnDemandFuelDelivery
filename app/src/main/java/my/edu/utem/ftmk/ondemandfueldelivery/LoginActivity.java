ff93c971bc5b17914be25aedbca756f3e42c46ad
Syaz <zanie455@gmail.com>
2022-01-16 00:12:17 +0800

    Created RegisterActivity

--- app/src/main/java/my/edu/utem/ftmk/ondemandfueldelivery/LoginActivity.java
+++ app/src/main/java/my/edu/utem/ftmk/ondemandfueldelivery/LoginActivity.java
@@ -2,8 +2,12 @@ package my.edu.utem.ftmk.ondemandfueldelivery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -12,10 +16,19 @@ public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
 
+    private EditText etEmail, etPassword;
+    private Button btLogin;
+    private TextView tvRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btLogin = findViewById(R.id.btLogin);
+        tvRegister = findViewById(R.id.tvRegister);
 
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
@@ -31,9 +44,10 @@ public class LoginActivity extends AppCompatActivity {
         }
     }
 
-    public void toMapActivity(View v){
+    public void toMapActivity(){
 
-
+        Intent intent = new Intent(LoginActivity.this, MapActivity.class);
+        startActivity(intent);
 
     }
 
